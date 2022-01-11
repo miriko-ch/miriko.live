dea384cd7257d423f963a9313a32e40933ac16ca
Hai Li <lonelyion@outlook.com>
2022-01-11 15:34:48 +0800

    frontend: candy box

--- pages/box.js
+++ pages/box.js
@@ -1,14 +1,46 @@
-import Head from "next/head"
-import { Container } from "react-bootstrap"
+import Head from "next/head";
+import { useState } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Circle } from "react-circle";
 
 export default function Box() {
-  return(
+  const [text, setText] = useState('');
+  const max_len = 1000;
+
+  return (
     <>
       <Head>
         <title>海离的提问箱</title>
       </Head>
-      <Container className="py-5 text-center" as="header">
-        提问箱desu
+      <Container className="py-5" as="header">
+        <Row className="py-lg-5">
+          <Col lg={8} md={10} className="mx-auto">
+            <Form>
+              <Form.Group className="mb-3" controlId="content">
+                <Row>
+                  <Col>
+                    <Form.Label>在此处填写想匿名和我说的话</Form.Label>
+                  </Col>
+                  { text.length != 0 &&
+                    <Col xs="auto" md="auto" lg="auto" style={ {textAlign: "right"} }>{' '}
+                      <Circle progress={ text.length != 0 ? Math.max((text.length / max_len) * 100, 5) : 0 } size="20" lineWidth="64" showPercentage={false} />
+                      <span style={{color: "darkgray"}}>{' '}{ max_len - text.length}</span>
+                    </Col>
+                  }
+                </Row>
+                <Form.Control as="textarea" 
+                              rows={5} maxLength={max_len} 
+                              onChange={(e) => setText(e.target.value)}
+                              placeholder="初见 可爱 单推 结婚"/>
+              </Form.Group>
+              <div className="text-center py-2">
+                <Button variant="primary" type="submit" className="text-center">
+                  提交棉花糖
+                </Button>
+              </div>
+            </Form>
+          </Col>
+        </Row>
       </Container>
     </>
   )
